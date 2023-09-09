1107bd570b045225e26bbefdddc507dd7d0c887f
iameazy <isrealmoses2020@gmail.com>
2023-09-09 04:12:00 -0700

    hhh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description: This function prints the size of various data types.
- * It uses the `sizeof` operator to determine the size in bytes of each data type
- * and displays the results.
- *
- * Return: 0 to indicate successful execution
- */
 int main(void)
 {
-	printf("Size of char: %zu bytes\n", sizeof(char));
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of short: %zu bytes\n", sizeof(short));
-	printf("Size of long: %zu bytes\n", sizeof(long));
-	printf("Size of long long: %zu bytes\n", sizeof(long long));
-	printf("Size of float: %zu bytes\n", sizeof(float));
-	printf("Size of double: %zu bytes\n", sizeof(double));
-	printf("Size of long double: %zu bytes\n", sizeof(long double));
+    printf("Size of char: %zu bytes\n", sizeof(char));
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of short: %zu bytes\n", sizeof(short));
+    printf("Size of long: %zu bytes\n", sizeof(long));
+    printf("Size of long long: %zu bytes\n", sizeof(long long));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+    printf("Size of long double: %zu bytes\n", sizeof(long double));
 
-	return (0);
+    #if __SIZEOF_POINTER__ == 4
+        printf("Architecture: 32-bit\n");
+    #elif __SIZEOF_POINTER__ == 8
+        printf("Architecture: 64-bit\n");
+    #else
+        printf("Architecture: Unknown\n");
+    #endif
+
+    return 0;
 }
