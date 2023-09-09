4e9698d3df6b20b015e6aa78c4a25005bb7cdb3c
iameazy <isrealmoses2020@gmail.com>
2023-09-09 03:38:40 -0700

    gss

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: Prints a message with proper grammar followed by a new line.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+int main() {
+    printf("Size of char: %zu bytes\n", sizeof(char));
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of short: %zu bytes\n", sizeof(short));
+    printf("Size of long: %zu bytes\n", sizeof(long));
+    printf("Size of long long: %zu bytes\n", sizeof(long long));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+    printf("Size of long double: %zu bytes\n", sizeof(long double));
+    
+    return 0;
 }
