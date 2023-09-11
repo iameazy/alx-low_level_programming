fa5da73a5a0130fe066b781e5c82afc50ed39a06
iameazy <isrealmoses2020@gmail.com>
2023-09-10 19:46:03 -0700

    ddd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,30 +10,14 @@
  */
 int main(void)
 {
-    printf("Size of char: %lu byte(s)\n", sizeof(char));
-    printf("Size of short: %lu byte(s)\n", sizeof(short));
-    printf("Size of int: %lu byte(s)\n", sizeof(int));
-    printf("Size of long: %lu byte(s)\n", sizeof(long));
-    printf("Size of long long: %lu byte(s)\n", sizeof(long long));
-    printf("Size of float: %lu byte(s)\n", sizeof(float));
-    printf("Size of double: %lu byte(s)\n", sizeof(double));
-    printf("Size of long double: %lu byte(s)\n", sizeof(long double));
+    printf("Size of char: %zu byte(s)\n", sizeof(char));
+    printf("Size of short: %zu byte(s)\n", sizeof(short));
+    printf("Size of int: %zu byte(s)\n", sizeof(int));
+    printf("Size of long: %zu byte(s)\n", sizeof(long));
+    printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+    printf("Size of float: %zu byte(s)\n", sizeof(float));
+    printf("Size of double: %zu byte(s)\n", sizeof(double));
+    printf("Size of long double: %zu byte(s)\n", sizeof(long double));
 
     return (0);
 }
-Size of char: 1 byte(s)
-Size of short: 2 byte(s)
-Size of int: 4 byte(s)
-Size of long: 4 byte(s)
-Size of long long: 8 byte(s)
-Size of float: 4 byte(s)
-Size of double: 8 byte(s)
-Size of long double: 12 byte(s)
-Size of char: 1 byte(s)
-Size of short: 2 byte(s)
-Size of int: 4 byte(s)
-Size of long: 8 byte(s)
-Size of long long: 8 byte(s)
-Size of float: 4 byte(s)
-Size of double: 8 byte(s)
-Size of long double: 16 byte(s)
