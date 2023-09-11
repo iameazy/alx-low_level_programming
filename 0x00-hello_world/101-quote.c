3ce48207a07e0b9e286f65b454bc9393a28a4ec8
iameazy <isrealmoses2020@gmail.com>
2023-09-10 17:35:12 -0700

    dgg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,24 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(void) {
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a specific message to the standard error
+ * and returns 1.
+ *
+ * Return: Always 1 (Error code)
+ */
+int main(void)
+{
     const char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
     ssize_t len = strlen(message);
-    ssize_t bytes_written = write(2, message, len);
+    ssize_t bytes_written = write(STDERR_FILENO, message, len);
 
-    if (bytes_written != len) {
+    if (bytes_written != len)
+    {
         return 1;
     }
 
-    return 0;
+    return 1;
 }
