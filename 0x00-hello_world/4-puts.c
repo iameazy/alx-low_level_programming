541edc8e093b61ceb332e558ee5d494d095d7293
iameazy <isrealmoses2020@gmail.com>
2023-09-10 21:33:48 -0700

    ddd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,12 @@
  *
  * Description: This function prints the message
  * "Programming is like building a multilingual puzzle" (including double quotes)
- * without a newline character.
+ * without a newline character and returns 0 to indicate successful execution.
  *
- * Return: 0 to indicate successful execution
+ * Return: 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\");
-    return 0;
+    puts("\"Programming is like building a multilingual puzzle\"");
+    return (0);
 }
