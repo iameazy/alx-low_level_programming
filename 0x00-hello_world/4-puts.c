3b7bd14d4a47a62e57dc4d80e12cc59fc8474c07
iameazy <isrealmoses2020@gmail.com>
2023-09-10 21:06:41 -0700

    hdhdh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -20,7 +20,7 @@ int main(void)
 	 * Use the puts function to print the message.
 	 * The '\n' character in the string ensures a newline after the message.
 	 */
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\"");
 
 	return (0); /* Return 0 to indicate successful execution */
 }
