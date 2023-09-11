7d5b3a5583f755c8986099bdf0e601e4a7fd780b
iameazy <isrealmoses2020@gmail.com>
2023-09-10 18:11:06 -0700

    hdhdhd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 #include <string.h>
-
 /**
  * main - Entry point of the program
  *
@@ -14,13 +13,10 @@ int main(void)
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = strlen(message);
 	ssize_t bytes_written;
-
 	bytes_written = write(STDERR_FILENO, message, len);
-
 	if (bytes_written != len)
 	{
 		return (1);
 	}
-
 	return (1);
 }
