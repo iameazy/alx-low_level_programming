bed467b4c0c666df2a1f13709ba6ada4c5f7f505
iameazy <isrealmoses2020@gmail.com>
2023-09-10 17:43:24 -0700

    nawanoooo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - Entry point of the program
  *
  * Description: This program prints a specific message to the standard error
- * and returns 1.
+ *              and returns 1.
  *
  * Return: Always 1 (Error code)
  */
@@ -13,12 +13,14 @@ int main(void)
 {
     const char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
     ssize_t len = strlen(message);
-    ssize_t bytes_written = write(STDERR_FILENO, message, len);
+    ssize_t bytes_written;
+
+    bytes_written = write(STDERR_FILENO, message, len);
 
     if (bytes_written != len)
     {
-        return 1;
+        return (1);
     }
 
-    return 1;
+    return (1);
 }
