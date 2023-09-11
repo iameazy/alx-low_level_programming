dbe9156af6a4679dedddc5ed4741f80531806ad7
iameazy <isrealmoses2020@gmail.com>
2023-09-10 20:59:53 -0700

    hdhdh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,26 @@
+/*
+ * File: main.c
+ * Author: Isreal
+ * Description: A simple C program that prints a message.
+ */
+
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
+ *
+ * Description: This function prints the message
+ * "Programming is like building a multilingual puzzle" followed by a newline
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	/*
+	 * Use the puts function to print the message.
+	 * The '\n' character in the string ensures a newline after the message.
+	 */
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0); /* Return 0 to indicate successful execution */
 }
