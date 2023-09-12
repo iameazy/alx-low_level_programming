63601a34b93c01e21ce61e555089bb8dd0c6eb09
iameazy <isrealmoses2020@gmail.com>
2023-09-11 18:07:41 -0700

    ddd

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints single-digit numbers in base 10
+ *              starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int digit;
+
+    for (digit = 0; digit < 10; digit++)
+    {
+        putchar(digit + '0'); // Print the digit as a character
+        putchar('\n');        // Print a newline character
+    }
+
+    return (0);
+}
