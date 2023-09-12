f592b6b5363b0441079a7b32ffaeca5a80940e65
iameazy <isrealmoses2020@gmail.com>
2023-09-11 18:29:15 -0700

    ggg

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all the numbers of base 16 in lowercase followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    /* Loop through the numbers from 0 to 15 (hexadecimal) */
+    for (i = 0; i < 16; i++)
+    {
+        char hexDigit;
+
+        /* Convert the integer to a lowercase hexadecimal character */
+        if (i < 10)
+            hexDigit = '0' + i;
+        else
+            hexDigit = 'a' + i - 10;
+
+        /* Use putchar to print the character */
+        putchar(hexDigit);
+    }
+
+    /* Print a newline character */
+    putchar('\n');
+
+    return (0);
+}
