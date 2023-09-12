784a31532cea79b97426cc496f8704cb7a329efb
iameazy <isrealmoses2020@gmail.com>
2023-09-11 18:18:41 -0700

    gfdd

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the lowercase alphabet
+ *              in reverse order, followed by a newline character.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     char c;
 
-    for (c = 'z'; c >= 'a'; c--) {
+    /* Loop through the lowercase alphabet in reverse order */
+    for (c = 'z'; c >= 'a'; c--)
+    {
+        /* Print the current character using putchar */
         putchar(c);
     }
-    
+
+    /* Print a newline character to end the line */
     putchar('\n');
 
-    return 0;
+    return (0);
 }
