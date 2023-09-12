e0dbc7dee665ea831a3a761d4472de3a62f983e7
iameazy <isrealmoses2020@gmail.com>
2023-09-11 19:05:30 -0700

    gdgdg

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,25 +11,25 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    /* Loop through single-digit numbers from 0 to 9 */
-    for (i = 0; i < 10; i++)
-    {
-        /* Print the current number as a character using putchar */
-        putchar('0' + i);
+	/* Loop through single-digit numbers from 0 to 9 */
+	for (i = 0; i < 10; i++)
+	{
+		/* Print the current number as a character using putchar */
+		putchar('0' + i);
 
-        /* Check if we've reached the last number */
-        if (i < 9)
-        {
-            /* Print the separator (", ") using putchar */
-            putchar(',');
-            putchar(' ');
-        }
-    }
+		/* Check if we've reached the last number */
+		if (i < 9)
+		{
+			/* Print the separator (", ") using putchar */
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-    /* Print a newline character to end the line */
-    putchar('\n');
+	/* Print a newline character to end the line */
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
