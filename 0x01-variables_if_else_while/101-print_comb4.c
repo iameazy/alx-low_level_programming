d2d7c597c487089f3bf02f72229c8eed3bdd5e46
iameazy <isrealmoses2020@gmail.com>
2023-09-11 19:22:08 -0700

    ddd

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
 
-int main() {
-    for (int i = 0; i <= 7; i++) {
-        for (int j = i + 1; j <= 8; j++) {
-            for (int k = j + 1; k <= 9; k++) {
-                // Convert integers to characters and print
-                putchar('0' + i);
-                putchar('0' + j);
-                putchar('0' + k);
+/**
+ * main - Prints all possible different combinations of three digits.
+ *
+ * Description: Numbers must be separated by ', ' followed by a space.
+ * The three digits must be different.
+ * Only the smallest combination of three digits is printed.
+ * Numbers are printed in ascending order with three digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
 
-                // Print ", " if not the last combination
-                if (!(i == 7 && j == 8 && k == 9)) {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				/* Convert integers to characters and print */
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
 
-    putchar('\n');
+				/* Print ", " if not the last combination */
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-    return 0;
+	putchar('\n');
+
+	return (0);
 }
