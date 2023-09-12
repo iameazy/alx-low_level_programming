9bb7d3820bc78398965ba3c36fa0bcbf69e5a93d
iameazy <isrealmoses2020@gmail.com>
2023-09-11 19:30:03 -0700

    dddd

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,30 +12,30 @@
  */
 int main(void)
 {
-    int i, j, k;
+	int i, j, k;
 
-    for (i = 0; i <= 7; i++)
-    {
-        for (j = i + 1; j <= 8; j++)
-        {
-            for (k = j + 1; k <= 9; k++)
-            {
-                /* Convert integers to characters and print */
-                putchar('0' + i);
-                putchar('0' + j);
-                putchar('0' + k);
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				/* Convert integers to characters and print */
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
 
-                /* Print ", " if not the last combination */
-                if (!(i == 7 && j == 8 && k == 9))
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+				/* Print ", " if not the last combination */
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
