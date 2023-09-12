54350469e26992ad5c5ef5d00c7141f99684efbf
iameazy <isrealmoses2020@gmail.com>
2023-09-11 19:08:41 -0700

    gggg

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i <= 9; i++) {
+        for (int j = i + 1; j <= 9; j++) {
+            putchar(i + '0');
+            putchar(j + '0');
+            if (i != 8 || j != 9) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+    return 0;
+}
