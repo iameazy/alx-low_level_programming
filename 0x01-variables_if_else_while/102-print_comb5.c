f1d6ba7a4290513414e4c42666adf6a926ce1606
iameazy <isrealmoses2020@gmail.com>
2023-09-11 19:49:22 -0700

    ggtr

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,37 +4,37 @@
  * main - Entry point of the program
  *
  * Description: Prints all possible combinations of two two-digit numbers
- * ranging from 00 00 to 99 99, including duplicates. Combinations are
+ * ranging from 00 01 to 99 99, including duplicates. Combinations are
  * separated by commas and spaces. Numbers are printed with two digits.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j;
+    int i, j;
 
-	for (i = 0; i <= 99; i++)
-	{
-		for (j = 0; j <= 99; j++)
-		{
-			/* Print the first two-digit number */
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
+    for (i = 0; i <= 99; i++)
+    {
+        for (j = 1; j <= 99; j++) // Start j from 01 to 99
+        {
+            /* Print the first two-digit number */
+            putchar((i / 10) + '0');
+            putchar((i % 10) + '0');
+            putchar(' ');
 
-			/* Print the second two-digit number */
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
+            /* Print the second two-digit number */
+            putchar((j / 10) + '0');
+            putchar((j % 10) + '0');
 
-			/* Print a comma and space if not the last combination */
-			if (i != 99 || j != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+            /* Print a comma and space if not the last combination */
+            if (!(i == 99 && j == 99))
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
-	putchar('\n'); /* Print a newline character at the end */
-	return (0);
+    putchar('\n'); /* Print a newline character at the end */
+    return (0);
 }
