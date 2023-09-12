306c3bfa59f3c6e50a51fe9f8c904156fddf4540
iameazy <isrealmoses2020@gmail.com>
2023-09-11 19:11:29 -0700

    hhfdd

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
-int main() {
-    for (int i = 0; i <= 9; i++) {
-        for (int j = i + 1; j <= 9; j++) {
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all possible different combinations
+ * of two digits, separated by a comma and space. It ensures that
+ * the two digits are different and prints only the smallest combination.
+ * The numbers are printed in ascending order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i, j;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = i + 1; j <= 9; j++)
+        {
             putchar(i + '0');
             putchar(j + '0');
-            if (i != 8 || j != 9) {
+
+            /* Check if this is not the last combination */
+            if (i != 8 || j != 9)
+            {
                 putchar(',');
                 putchar(' ');
             }
         }
     }
+
     putchar('\n');
-    return 0;
+    return (0);
 }
