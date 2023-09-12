2e685b104e0ed1807a2bf6e36c2aa056c4fb8ed2
iameazy <isrealmoses2020@gmail.com>
2023-09-11 17:49:13 -0700

    hhg

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Print Single-Digit Numbers in Base 10
+"""
+
+def main():
+    """
+    Main function to print single-digit numbers in base 10.
+    """
+    for i in range(10):
+        print(i)
+
+if __name__ == "__main__":
+    main()
