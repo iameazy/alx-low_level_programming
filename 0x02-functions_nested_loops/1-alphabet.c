4e922a65ddc1cce5ae6833728e936d41a74ec563
iameazy <isrealmoses2020@gmail.com>
2023-09-12 17:21:31 -0700

    dddd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,29 @@
-#include <unistd.h>
+#include <stdio.h>
+#include "alphabet.h" // Include the header file where print_alphabet is defined
 
-/**
- * print_alphabet - Prints the lowercase alphabet followed by a new line.
- */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+#ifndef ALPHABET_H
+#define ALPHABET_H
+
+void print_alphabet(void);
+
+#endif /* ALPHABET_H */
+#include "alphabet.h" // Include the header file where print_alphabet is declared
+
 void print_alphabet(void)
 {
-	char letter = 'a';
+    char letter = 'a';
 
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
 
-	_putchar('\n');
+    putchar('\n');
 }
 
