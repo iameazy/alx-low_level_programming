2a72d48e5dfa39af409e6da446d9e4080597d307
iameazy <isrealmoses2020@gmail.com>
2023-09-18 15:12:31 -0700

    fff

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,22 +6,16 @@
  *
  * @str: The input string to be printed in reverse.
  */
-void _print_reverse(char *str)
-{
-    int length; /* Declare 'int i' here in ISO C90 */
-    if (str == NULL)
-    {
-        return;  /* Handle NULL input */
+void _print_reverse(char *s) {
+    int length = 0;
+    int i; /* Move the declaration here */
+    while (s[length] != '\0') {
+        length++;
     }
 
-    length = strlen(str);
-
-    int i;
-    for (i = length - 1; i >= 0; i--)
-
-    {
-        putchar(str[i]);
+    for (i = length - 1; i >= 0; i--) {
+        putchar(s[i]);
     }
-
     putchar('\n');
 }
+
