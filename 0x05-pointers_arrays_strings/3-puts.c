80a2a28db867594026c42ef3d336ded687d7006c
iameazy <isrealmoses2020@gmail.com>
2023-09-18 14:30:02 -0700

    ddd

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ *
+ * @str: The input string to be printed.
+ */
+void _puts(char *str)
+{
+    if (str != NULL)
+    {
+        printf("%s\n", str);
+    }
+}
+
