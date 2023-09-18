07afadb386e1506bfdf9362f60ce87085dbb8058
iameazy <isrealmoses2020@gmail.com>
2023-09-18 14:21:44 -0700

    :ddd:

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,14 +9,14 @@
  */
 int _strlen(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    /* Loop through the characters of the string until null terminator is reached */
-    while (*s != '\0')
-    {
-        length++;
-        s++;
-    }
+	/* Loop through the characters of the string until null terminator is reached */
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
 
-    return length;
+	return (length);
 }
