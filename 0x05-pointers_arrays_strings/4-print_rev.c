aa06b111bfbd605b5f10eb342a66f1094c6da548
iameazy <isrealmoses2020@gmail.com>
2023-09-18 16:03:34 -0700

    ddd

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print_rev(char *s) {
+    int length = 0;
+    while (s[length] != '\0') {
+        length++;
+    }
+
+    for (int i = length - 1; i >= 0; i--) {
+        putchar(s[i]);
+    }
+    putchar('\n');
+}
