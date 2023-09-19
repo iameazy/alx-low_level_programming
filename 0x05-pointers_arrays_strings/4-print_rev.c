86991fbeaa843f6de9f9842edaab307389fbef46
iameazy <isrealmoses2020@gmail.com>
2023-09-18 17:20:51 -0700

    ggg

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,14 @@
  */
 void print_rev(char *s)
 {
-    int length = strlen(s);
-    int i;
+	int length = strlen(s);
+	int i;
 
-    for (i = length - 1; i >= 0; i--)
-    {
-        printf("%c", s[i]);
-    }
+	for (i = length - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
 
-    printf("\n");
+	printf("\n");
 }
+
