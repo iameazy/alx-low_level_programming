29e25b4a992bbdfb9c75004d2ea84b37a51e29ff
iameazy <isrealmoses2020@gmail.com>
2023-09-18 18:44:42 -0700

    hf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,19 +10,19 @@
  */
 void print_array(int *a, int n)
 {
-	if (n <= 0)
-	{
-		printf("\n");  /* Print a newline for an empty array or invalid input */
-		return;
-	}
+    int i; /* Declare loop variable outside of the loop */
 
-	int i; /* Declare loop variable outside of the loop */
+    if (n <= 0)
+    {
+        printf("\n");  /* Print a newline for an empty array or invalid input */
+        return;
+    }
 
-	for (i = 0; i < n - 1; i++)
-	{
-		printf("%d, ", a[i]);
-	}
+    for (i = 0; i < n - 1; i++)
+    {
+        printf("%d, ", a[i]);
+    }
 
-	printf("%d\n", a[n - 1]);  /* Print the last element followed by a newline */
+    printf("%d\n", a[n - 1]);  /* Print the last element followed by a newline */
 }
 
