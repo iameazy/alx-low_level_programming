048e56e8fb7ae2c2ca034c07f82ecbad781eedb7
iameazy <isrealmoses2020@gmail.com>
2023-09-18 19:16:14 -0700

    hgdd

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,7 +4,8 @@
  * _strcpy - Copies a string from src to dest, including the null terminator.
  *
  * @dest: A pointer to the destination buffer.
- * @src: A pointer to the source string to be copied. 
+ * @src: A pointer to the source string to be copied.
+ *
  * Return: A pointer to the destination buffer (dest).
  */
 char *_strcpy(char *dest, const char *src)
@@ -21,5 +22,6 @@ char *_strcpy(char *dest, const char *src)
 
 	/* Add the null terminator at the end of dest */
 	*dest = '\0';
-        return (original_dest); /* Return the original destination pointer */
+
+	return (original_dest); /* Return the original destination pointer */
 }
