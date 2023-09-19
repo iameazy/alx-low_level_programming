b3421e8230ab3112d2d192755adb5f469e10ad7e
iameazy <isrealmoses2020@gmail.com>
2023-09-18 18:40:51 -0700

    hgf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,10 +16,13 @@ void print_array(int *a, int n)
 		return;
 	}
 
-	for (int i = 0; i < n - 1; i++)
+	int i; /* Declare loop variable outside of the loop */
+
+	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d, ", a[i]);
 	}
 
 	printf("%d\n", a[n - 1]);  /* Print the last element followed by a newline */
 }
+
