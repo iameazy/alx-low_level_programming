744b6221df209c117cb2b27dcf33adfde74054ae
iameazy <isrealmoses2020@gmail.com>
2023-09-18 19:28:29 -0700

    ddd

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,38 +9,38 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1; /* Default sign is positive */
+    int result = 0;
+    int sign = 1; /* Default sign is positive */
 
-	/* Skip leading non-numeric characters */
-	while (*s && (*s < '0' || *s > '9') && (*s != '-' && *s != '+'))
-		s++;
+    /* Skip leading non-numeric characters */
+    while (*s && (*s < '0' || *s > '9') && (*s != '-' && *s != '+'))
+        s++;
 
-	/* Check for sign (+ or -) */
-	if (*s == '-')
-	{
-		sign = -1;
-		s++;
-	}
-	else if (*s == '+')
-	{
-		s++;
-	}
+    /* Check for sign (+ or -) */
+    if (*s == '-')
+    {
+        sign = -1;
+        s++;
+    }
+    else if (*s == '+')
+    {
+        s++;
+    }
 
-	/* Convert the string to an integer */
-	while (*s >= '0' && *s <= '9')
-	{
-		/* Check for integer overflow */
-		if (result > (INT_MAX - (*s - '0')) / 10)
-		{
-			if (sign == 1)
-				return INT_MAX;
-			else
-				return INT_MIN;
-		}
-		result = result * 10 + (*s - '0');
-		s++;
-	}
+    /* Convert the string to an integer */
+    while (*s >= '0' && *s <= '9')
+    {
+        /* Check for integer overflow */
+        if (result > (INT_MAX - (*s - '0')) / 10)
+        {
+            if (sign == 1)
+                return (INT_MAX);
+            else
+                return (INT_MIN);
+        }
+        result = result * 10 + (*s - '0');
+        s++;
+    }
 
-	return result * sign;
+    return (result * sign);
 }
