2031b1d34287730aa94385550da80483d5489ae0
iameazy <isrealmoses2020@gmail.com>
2023-09-18 19:20:41 -0700

    hy

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * _atoi - Convert a string to an integer.
+ * @s: The input string.
+ *
+ * Return: The integer value of the string.
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1; /* Default sign is positive */
+
+	/* Skip leading white spaces */
+	while (*s == ' ')
+		s++;
+
+	/* Check for sign (+ or -) */
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+
+	/* Convert the string to an integer */
+	while (*s >= '0' && *s <= '9')
+	{
+		/* Check for integer overflow */
+		if (result > (INT_MAX - (*s - '0')) / 10)
+		{
+			if (sign == 1)
+				return INT_MAX;
+			else
+				return INT_MIN;
+		}
+		result = result * 10 + (*s - '0');
+		s++;
+	}
+
+	return result * sign;
+}
