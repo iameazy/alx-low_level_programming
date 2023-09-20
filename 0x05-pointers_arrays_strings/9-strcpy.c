da1192761b285b72383164348b0f9909ab4d1a2d
iameazy <isrealmoses2020@gmail.com>
2023-09-19 17:40:58 -0700

    ddd

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,18 +9,17 @@
  */
 char *_strcpy(char *dest, const char *src)
 {
-    char *original_dest = dest; /* Store the original destination pointer */
+	char *original_dest = dest; /* Store the original destination pointer */
 
-    /* Copy characters from src to dest until we reach the null terminator */
-    while (*src != '\0')
-    {
-        *dest = *src; /* Copy a character */
-        dest++;       /* Move the destination pointer forward */
-        src++;        /* Move the source pointer forward */
-    }
+	/* Copy characters from src to dest until we reach the null terminator */
+	while (*src != '\0')
+	{
+		*dest = *src; /* Copy a character */
+		dest++;       /* Move the destination pointer forward */
+		src++;        /* Move the source pointer forward */
+	}
 
-    *dest = '\0'; /* Add the null terminator to dest */
+	*dest = '\0'; /* Add the null terminator to dest */
 
-    return original_dest; /* Return the original destination pointer */
-}
+	return original_dest; /* Return the original destination pointer */
 
