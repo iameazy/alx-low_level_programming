ff2d72f4fa130f64fb2f5c0047da752c36a121bd
iameazy <isrealmoses2020@gmail.com>
2023-09-19 17:44:12 -0700

    ddd

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+/* Function prototype */
+char *_strcpy(char *dest, const char *src);
+
+int main(void)
+{
+    char source[] = "Hello, World!";
+    char destination[20]; /* Make sure destination has enough space */
+
+    char *result = _strcpy(destination, source);
+
+    printf("Source: %s\n", source);
+    printf("Destination: %s\n", destination);
+
+    return 0;
+}
+
 /**
  * _strcpy - Copies a string from src to dest.
  * @dest: Pointer to the destination buffer.
@@ -9,17 +25,17 @@
  */
 char *_strcpy(char *dest, const char *src)
 {
-	char *original_dest = dest; /* Store the original destination pointer */
+    char *original_dest = dest; /* Store the original destination pointer */
 
-	/* Copy characters from src to dest until we reach the null terminator */
-	while (*src != '\0')
-	{
-		*dest = *src; /* Copy a character */
-		dest++;       /* Move the destination pointer forward */
-		src++;        /* Move the source pointer forward */
-	}
+    /* Copy characters from src to dest until we reach the null terminator */
+    while (*src != '\0')
+    {
+        *dest = *src; /* Copy a character */
+        dest++;       /* Move the destination pointer forward */
+        src++;        /* Move the source pointer forward */
+    }
 
-	*dest = '\0'; /* Add the null terminator to dest */
+    *dest = '\0'; /* Add the null terminator to dest */
 
-	return original_dest; /* Return the original destination pointer */
-
+    return original_dest; /* Return the original destination pointer */
+}
