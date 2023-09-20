5aa33d51651d76af8c5045733167834ede8bfbe8
iameazy <isrealmoses2020@gmail.com>
2023-09-19 17:03:23 -0700

    hdhdhd

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,15 @@
  */
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        putchar(str[i]);
-        i++;
-        if (str[i] != '\0') /* Check if the next character exists */
-            i++; /* Increment i to skip the next character */
-    }
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+		if (str[i] != '\0') /* Check if the next character exists */
+			i++; /* Increment i to skip the next character */
+	}
 
-    putchar('\n'); /* Print a new line after all characters are printed */
+	putchar('\n'); /* Print a new line after all characters are printed */
 }
