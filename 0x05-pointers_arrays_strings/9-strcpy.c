1455fa3a18cb9a9b02bcbe9303b593c63400500a
iameazy <isrealmoses2020@gmail.com>
2023-09-19 17:37:24 -0700

    ddd

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
 /**
- * _strcpy - Copies a string from src to dest, including the null terminator.
+ * _strcpy - Copies a string from src to dest.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string to be copied.
  *
- * @dest: A pointer to the destination buffer.
- * @src: A pointer to the source string to be copied.
- *
- * Return: A pointer to the destination buffer (dest).
+ * Return: A pointer to the destination buffer.
  */
 char *_strcpy(char *dest, const char *src)
 {
-	char *original_dest = dest; /* Store the original destination pointer */
+    char *original_dest = dest; /* Store the original destination pointer */
 
-	/* Copy characters from src to dest until the null terminator is encountered */
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
+    /* Copy characters from src to dest until we reach the null terminator */
+    while (*src != '\0')
+    {
+        *dest = *src; /* Copy a character */
+        dest++;       /* Move the destination pointer forward */
+        src++;        /* Move the source pointer forward */
+    }
 
-	/* Add the null terminator at the end of dest */
-	*dest = '\0';
+    *dest = '\0'; /* Add the null terminator to dest */
 
-	return (original_dest); /* Return the original destination pointer */
+    return original_dest; /* Return the original destination pointer */
 }
+
